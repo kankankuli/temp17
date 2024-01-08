3e55dd0983a166a36378be3fdad66130e90fba1b
kankankuli <kankankuli@gmail.com>
2024-01-08 16:17:30 +0800

    Add files via upload

---
+++ pos_pads_location/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Pos Pads Location",
+    "author": "IdeaCode Academy",
+    "depends": ['point_of_sale'],
+    "images": ['static/description/banner.png'],
+    "assets": {
+        'point_of_sale._assets_pos': [
+            "pos_pads_location/static/src/**/*"
+        ],
+    },
+    "license": "LGPL-3"
+}
