2907b59451fa4180fce29b62ed791a9381b22991
kankankuli <kankankuli@gmail.com>
2024-02-18 18:46:27 +0800

    Add files via upload

---
+++ Pos_Product_List_Limit/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Pos Product List Limit',
+    'version': '17.0',
+    'category': 'Point of Sale',
+    'summary': 'Pos Product List Limit 5',
+    'description': """
+       
+      Pos Product List Limitxx
+    
+    """,
+    'author': 'aa',
+    'website': '',
+    'depends': ['base', 'point_of_sale'],
+    'data': [
+ 
+    ],
+    'assets': {
+
+        'point_of_sale._assets_pos': [
+
+            'Pos_Product_List_Limit/static/src/app/store/db.js',
+        ],
+        
+    },
+    'license':'OPL-1',
+    'installable': True,
+    'auto_install': False,
+}
