b425e00cb77d017bf225f08db3d04743bfefb38e
kankankuli <kankankuli@gmail.com>
2024-02-24 20:12:49 +0800

    Create abc.js

---
+++ pos_add_product_barcode_no_reload/static/src/js/abc.js
@@ -0,0 +1,7 @@
+odoo.define("pos_search_limit.db", function(require) {
+    "use strict";
+    var PosDB = require("point_of_sale.DB");
+    PosDB.include({
+        limit: 4, // The maximum number of results returned by a search
+    });
+});
